4cacf2a2efc7b2277583ab535f1f4b7b8f3aef94
Tugay Gül <sonder.tg@gmail.com>
2019-06-10 23:24:18 +0300

    add a new line

--- q1.py
+++ q1.py
@@ -11,15 +11,3 @@ for i in range(2000, 3201):
 print(','.join(l))
 
 
-# With a given integral number n, write a program to generate a dictionary that contains (i, i*i)
-# such that is an integral number between 1 and n (both included). and then the program should print the dictionary.
-# Suppose the following input is supplied to the program:
-# 8
-# Then, the output should be:
-# {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
-
-n = int(input())
-d = dict()
-for i in range (1,n+1):
-    d[i]=i*i
-print(d)
