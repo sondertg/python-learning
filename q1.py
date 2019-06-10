46d53a2969ef184912c3c594054b3feb862c62d6
Tugay Gül <sonder.tg@gmail.com>
2019-06-10 22:48:26 +0300

    solving question 2

--- q1.py
+++ q1.py
@@ -1,10 +1,42 @@
 # Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
 # between 2000 and 3200 (both included).
 # The numbers obtained should be printed in a comma-separated sequence on a single line.
+from typing import List
 
-l=[]
+l: List[str]=[]
 for i in range(2000, 3201):
     if (i%7==0) and (i%5!=0):
         l.append(str(i))
 
 print(','.join(l))
+
+# Question:
+# Write a program which can compute the factorial of a given numbers.
+# The results should be printed in a comma-separated sequence on a single line.
+# Suppose the following input is supplied to the program:
+# 8
+# Then, the output should be:
+# 40320
+
+
+def fact(x):
+    if x == 0:
+        return 1
+    return x * fact(x - 1)
+
+
+x = int(input("write a positive number to calculate its factorial"))
+print("factorial of x is", fact(x))
+
+# With a given integral number n, write a program to generate a dictionary that contains (i, i*i)
+# such that is an integral number between 1 and n (both included). and then the program should print the dictionary.
+# Suppose the following input is supplied to the program:
+# 8
+# Then, the output should be:
+# {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+n = int(input())
+d = dict()
+for i in range (1,n+1):
+    d[i]=i*i
+print(d)
